1baf27c33a94748f431b340a4ce2b2c9000ae80b
gabriel moura <gcdm@ecomp.poli.br>
2017-10-19 20:39:43 -0300

    Referencias feitas em sala

---
+++ Referencias/teste.h
@@ -0,0 +1,26 @@
+#ifndef TESTE_H
+#define TESTE_H
+//tudo no .h é inline;
+//delete[] quando o new é dado para um array ex new int[]; ver abaixo
+//delete apenas para new quando malloc é free;
+class teste{
+	private:
+		int a_;
+		float b_;
+		double* c_;
+	public:
+		teste():
+			a_(0),b_(0.0f){}
+		teste(int a,float b):
+			a_(a),b_(b),c(new double[8])){}
+		virtual	~teste(){
+				delete[] c;
+			}
+		teste(const TESTE& t): a_(t.a_), b_(t.b),c_(t.c_){}
+        void set_a(int a){
+            a_ = a;
+		int a() const /*não modifica a instancia da classe*/ {
+			return a_;
+		}		
+}
+#endif
